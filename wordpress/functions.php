7c0530adff689acfacda27f65142eaf257a75238
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2025-06-05 07:10:08 -0700

    Add files via upload

---
+++ wordpress/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function load_scripts(){
+    wp_enqueue_style(
+        'style',
+        get_stylesheet_uri(),
+        array(),
+        filemtime(get_template_directory().'/style.css'),'all'
+        );
+}
+        add_action('wp_enqueue_scripts','load_scripts');
+
+        ?>
