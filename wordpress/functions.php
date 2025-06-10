c58b9ec7e79160418a3462742869aa046d1e80b1
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2025-06-10 07:06:30 -0700

    Update functions.php

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -10,4 +10,33 @@ function load_scripts(){
 }
         add_action('wp_enqueue_scripts','load_scripts');
 
+
+        function config() {
+            register_nav_menus(
+                array(
+                    'wp_devs_main_menu'=>'Main Menu',
+                    'wp_devs_footer_menu'=>'footer Menu'
+                )
+                );
+
+                $args=array(
+                    'height'=>225,
+                    'width'=>1920
+                );
+
+                add_theme_support('custom-header',$args);
+                add_theme_support('post_thumbnails');
+                add_theme_support('custom-logo',array(
+                    'width'=>200,
+                    'height'=>110,
+                    'flex-height'=>true,
+                    'flex-width'=>true
+                ));
+                add_theme_support('automatic-feed-links');
+                add_theme_support('html5',array('comment-list','comment-form','search-form','gallery','caption','style','script'));
+                add_theme_support('title-tag');
+        }
+
+        add_action('after_setup_theme','config',0);
+
         ?>
