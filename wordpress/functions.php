8ce7f986f9b321289b8f9c6fc638535e9d982526
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2025-06-17 07:03:08 -0700

    Update functions.php

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -7,6 +7,11 @@ function load_scripts(){
         array(),
         filemtime(get_template_directory().'/style.css'),'all'
         );
+
+        wp_enqueue_style('bootstrap',get_template_directory_uri().'/css/bootstrap.min.css');
+        wp_enqueue_script('dropdown',get_template_directory().'/js/dropdown.js',array(),'1.0',false);
+        wp_enqueue_style('bootstrap2',get_template_directory().'/js/bootstrap.bundle.min.js',array('jquery'));
+
 }
         add_action('wp_enqueue_scripts','load_scripts');
 
