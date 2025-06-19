df4e777c34ebff6aca6157301e9bed1f1687d460
olsaa0 <166027576+olsaa0@users.noreply.github.com>
2025-06-19 07:02:58 -0700

    Create front-page.php

---
+++ wordpress/front-page.php
@@ -0,0 +1,16 @@
+<?php get_header();?>
+<div id="container">
+    <div id="inner_container">
+        <div class="imgs_container">
+            <div class="img_container">
+                <img src="<?php echo get_template_directory_uri();?>/images/download.jpg">
+</div>
+<div class="img_container">
+                <img src="<?php echo get_template_directory_uri();?>/images/download.jpg">
+            </div>
+<div class="img_container">
+                <img src="<?php echo get_template_directory_uri();?>/images/download.jpg">
+            </div>
+        </div>
+    </div>
+</div>
